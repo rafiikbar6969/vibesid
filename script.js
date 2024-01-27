dbb7713da73033e92d749512a70567e4ac431707
uwurap <passeo.012345@gmail.com>
2024-01-28 01:44:41 +0800

    update header ama svg

--- script.js
+++ script.js
@@ -1,77 +1,84 @@
+$(window).scroll(function () {
+  // bagian header
+  var wScroll = $(this).scrollTop();
+  $(".header h1").css({
+    transform: "translate(0px," + wScroll / 2 + "%)",
+  });
+  $(".header p").css({
+    transform: "translate(0px," + wScroll / 1.2 + "%)",
+  });
+  // akhir bagian header
 
+  // program
+  if (wScroll > $("section.program").offset().top - 100) {
+    $(".service .layanan").addClass("muncul");
+  }
+  if (wScroll < 100) {
+    $(".service .layanan").removeClass("muncul");
+  }
+  // end program
 
-$(window).scroll( function(){
-    // bagian header
-    var wScroll = $(this).scrollTop();
-    $('.header h1').css({
-        'transform' : 'translate(0px,'+ wScroll/2 +'%)'
-    });
-    $('.header p').css({
-        'transform' : 'translate(0px,'+ wScroll/1.2 +'%)'
-    });
-    // akhir bagian header
+  // about
+  if (wScroll > $("section.about").offset().top - 100) {
+    $(".left-about").addClass("about-active");
+  }
+  if (wScroll > $("section.about").offset().top - 100) {
+    $(".right-about").addClass("about-active");
+  }
+  if (wScroll < 100) {
+    $(".right-about").removeClass("about-active");
+    console.log("ok");
+  }
+  if (wScroll < 100) {
+    $(".left-about").removeClass("about-active");
+  }
+  // end about
 
+  // feature
+  let cards = document.querySelectorAll(".card");
+  let stackArea = document.querySelector(".stack-area");
 
-
-    // program
-    if(wScroll > $('section.program').offset().top -100){
-        $('.service .layanan').addClass('muncul');
-    }
-    if(wScroll < 100){
-        $('.service .layanan').removeClass('muncul');
-    }
-    // end program
-
-
-    // about
-    if(wScroll > $('section.about').offset().top -100){
-        $('.left-about').addClass('about-active');
-    }
-    if(wScroll > $('section.about').offset().top -100){
-        $('.right-about').addClass('about-active');
-    }
-    if(wScroll < 100){
-        $('.right-about').removeClass('about-active');
-        console.log('ok')
-    }
-    if(wScroll < 100){
-        $('.left-about').removeClass('about-active');
-    }
-    // end about
-
-    // feature
-    let cards = document.querySelectorAll(".card");
-    let stackArea = document.querySelector(".stack-area");
-
-    function rotateCards() {
-      let angle = 0;
-      cards.forEach((card) => {
-        if (card.classList.contains("active")) {
-          card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
-        } else {
-          card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-          angle = angle - 10;
-        }
-      });
-    }
-
-    rotateCards();
-
-    window.addEventListener("scroll", () => {
-      let proportion =
-        stackArea.getBoundingClientRect().top / window.innerHeight;
-      if (proportion <= 0) {
-        let n = cards.length;
-        let index = Math.ceil((proportion * n) / 2);
-        index = Math.abs(index) - 1;
-        for (let i = 0; i < n; i++) {
-          if (i <= index) {
-            cards[i].classList.add("active");
-          } else {
-            cards[i].classList.remove("active");
-          }
-        }
-        rotateCards();
+  function rotateCards() {
+    let angle = 0;
+    cards.forEach((card) => {
+      if (card.classList.contains("active")) {
+        card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
+      } else {
+        card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+        angle = angle - 10;
       }
     });
+  }
+
+  rotateCards();
+
+  window.addEventListener("scroll", () => {
+    let proportion = stackArea.getBoundingClientRect().top / window.innerHeight;
+    if (proportion <= 0) {
+      let n = cards.length;
+      let index = Math.ceil((proportion * n) / 2);
+      index = Math.abs(index) - 1;
+      for (let i = 0; i < n; i++) {
+        if (i <= index) {
+          cards[i].classList.add("active");
+        } else {
+          cards[i].classList.remove("active");
+        }
+      }
+      rotateCards();
+    }
+  });
+});
+
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(items[0]);
+});
+
+prev.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]); // here the length of items = 6
 });
